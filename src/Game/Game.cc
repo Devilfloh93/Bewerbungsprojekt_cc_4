2b3ffe01eee1e4566532cb119f1ffbef02db679f
Devilfloh <poetzschflorian@gmxde>
2024-02-16 16:05:24 +0100

    update

--- src/Game/Game.cc
+++ src/Game/Game.cc
@@ -1,1 +1,15 @@
 #include "Game.h"
+
+Game::Game()
+{
+}
+
+GameState Game::GetGameState() const
+{
+    return this->m_gameState;
+}
+
+void Game::SetGameState(const GameState gameState)
+{
+    this->m_gameState = gameState;
+}
--- src/Player/Player.cc
+++ src/Player/Player.cc
@@ -1,1 +1,26 @@
 #include "Player.h"
+
+Player::Player(const std::string name, const float health, const float water, const float food)
+    : m_name(name), m_health(health), m_water(water), m_food(food)
+{
+}
+
+std::string Player::GetName() const
+{
+    return this->m_name;
+}
+
+float Player::GetHealth() const
+{
+    return this->m_health;
+}
+
+float Player::GetWater() const
+{
+    return this->m_water;
+}
+
+float Player::GetFood() const
+{
+    return this->m_food;
+}
